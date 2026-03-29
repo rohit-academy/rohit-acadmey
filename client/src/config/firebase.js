303852c33497f7e6afc8a5b2fcb81d3b87d30477
lordluci4er <lordluci4er@gmail.com>
2026-03-29 14:28:45 +0530

    🔥 Updated Firebase config (new project)

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
    🔥 FIREBASE CONFIG (SINGLE SOURCE)
 ===================================== */
 const firebaseConfig = {
-  apiKey: "AIzaSyDr3ReSW7oZ7PiuQep38XNzmlTzPmoHqSM",
-  authDomain: "rohit-academy16.firebaseapp.com",
-  projectId: "rohit-academy16",
-  storageBucket: "rohit-academy16.firebasestorage.app",
-  messagingSenderId: "541264278096",
-  appId: "1:541264278096:web:6df00f6b8ffddd12d09755",
+  apiKey: "AIzaSyBw1LyyJrNdem3SrhmImnxvWfoM1UZpagI",
+  authDomain: "rohit-academy2026.firebaseapp.com",
+  projectId: "rohit-academy2026",
+  storageBucket: "rohit-academy2026.firebasestorage.app",
+  messagingSenderId: "498241904742",
+  appId: "1:498241904742:web:81ba247bdf9090eb494da7",
 };
 
 /* =====================================
